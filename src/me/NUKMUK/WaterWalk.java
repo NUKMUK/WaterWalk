55c5ff3b8bbde5f45e9efc6be2dbd74e54ec7162
NUKMUK <rasmus.hintzmadsen@gmail.com>
2015-12-30 17:39:39 +0200

    Initial commit

---
+++ src/me/NUKMUK/WaterWalk.java
@@ -0,0 +1,56 @@
+package me.NUKMUK;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rasmu on 22.12.2015.
+ */
+@SuppressWarnings("deprecation")
+public class WaterWalk extends JavaPlugin implements Listener {
+
+    public void onEnable(){
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    public static List<Block> getNearbyBlocks(Location location, int radius) {
+        List<Block> blocks = new ArrayList<Block>();
+        for(int x = location.getBlockX() - radius; x <= location.getBlockX() + radius; x++) {
+            for(int y = location.getBlockY() - radius; y <= location.getBlockY() + radius; y++) {
+                for(int z = location.getBlockZ() - radius; z <= location.getBlockZ() + radius; z++) {
+                    blocks.add(location.getWorld().getBlockAt(x, y, z));
+                }
+            }
+        }
+        return blocks;
+    }
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent e) {
+        if (e.getTo().getBlock().getLocation().add(0, -1, 0).getBlock().getType() == Material.STATIONARY_WATER) {
+            for (Block b : getNearbyBlocks(e.getPlayer().getLocation(), 1)) {
+                if (b.getLocation().getY() < e.getPlayer().getLocation().getY()) {
+                    if (b.getType() == Material.STATIONARY_WATER) {
+                        if (Math.random() < 0.5) {
+                            e.getPlayer().sendBlockChange(e.getTo().getBlock().getLocation().add(0, -1, 0), Material.STAINED_CLAY, (byte) (Math.random() * 16));
+                            e.getPlayer().sendBlockChange(e.getTo().getBlock().getLocation().add(0, -2, 0), Material.STAINED_CLAY, (byte) (Math.random() * 16));
+                            if((b.getLocation() != e.getPlayer().getLocation())){
+                                e.getPlayer().sendBlockChange(b.getLocation(), Material.STAINED_GLASS, (byte) (Math.random() * 16));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
